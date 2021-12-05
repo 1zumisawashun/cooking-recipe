e6d128fbc12d0cd3762acbcc9c5efc33c5104aff
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-05 14:22:25 +0900

    add-recipe-on-json-server

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,30 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url, method = "GET") => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
+  const [options, setOptions] = useState(null);
+
+  const postData = (postData) => {
+    setOptions({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    });
+  };
 
   useEffect(() => {
     const controller = new AbortController();
-    const fecthData = async () => {
+    const fecthData = async (fetchOptions) => {
       setIsPending(true);
       try {
-        const res = await fetch(url, { signal: controller.signal });
+        const res = await fetch(url, {
+          ...fetchOptions,
+          signal: controller.signal,
+        });
         if (!res.ok) {
           throw new Error(res.statusText);
         }
@@ -27,10 +41,15 @@ export const useFetch = (url) => {
         }
       }
     };
-    fecthData();
+    if (method === "GET") {
+      fecthData();
+    }
+    if (method === "POST" && options) {
+      fecthData(options);
+    }
     return () => {
       controller.abort();
     };
-  }, [url]);
-  return { data, isPending, error };
+  }, [url, options, method]);
+  return { data, isPending, error, postData };
 };
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,6 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 import "./Create.css";
 
 export default function Create() {
@@ -8,10 +10,21 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
+  const history = useHistory();
+
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime, ingredients);
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + "minutes",
+    });
   };
 
   const handleAdd = (e) => {
@@ -25,6 +38,14 @@ export default function Create() {
     setNewIngredient("");
     ingredientInput.current.focus();
   };
+
+  // redirect user
+  useEffect(() => {
+    if (data) {
+      history.push("/");
+    }
+  }, [data]);
+  // FIXME: React Hook useEffect has a missing dependency: 'history'. Either include it or remove the dependency array.
 
   return (
     <div className="create">
